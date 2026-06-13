f08eada5295a78a29b18a956d000fee196752015
Dylan Forbes <dylandforbes@gmail.com>
2026-06-13 11:14:29 -0700

    another fix for withRLS: actually run the provided function, instead of returning a lambda. duh

--- packages/verso/src/userland/testing/rls.ts
+++ packages/verso/src/userland/testing/rls.ts
@@ -1,23 +1,21 @@
 import {stopClientRLS, startClientRLS, runWithServerRLS} from "../../core/common/RequestLocalStorage";
 import type {MaybePromise} from "../../util/promise";
 
-export function withRLS<R, P extends MaybePromise<R>>(fn: () => P): () => P {
+export function withRLS<R, P extends MaybePromise<R>>(fn: () => P): P {
   if (globalThis.IS_SERVER) {
-    return () => runWithServerRLS(fn);
+    return runWithServerRLS(fn);
   }
-  return () => {
-    startClientRLS();
-    let result: P;
-    try {
-      result = fn();
-    } catch (e) {
-      stopClientRLS();
-      throw e;
-    }
-    if (result instanceof Promise) {
-      return result.finally(stopClientRLS) as P;
-    }
+  startClientRLS();
+  let result: P;
+  try {
+    result = fn();
+  } catch (e) {
     stopClientRLS();
-    return result;
-  };
+    throw e;
+  }
+  if (result instanceof Promise) {
+    return result.finally(stopClientRLS) as P;
+  }
+  stopClientRLS();
+  return result;
 }
